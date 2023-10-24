747f33d56449274dada9f17b00c90ab10fd7f0f3
Nico Taboada <nico@prokeep.com>
2023-10-23 21:09:18 -0300

    agrego files para poder hacer deploy del back

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -7,22 +7,22 @@ import { resolvers as scalarResolvers } from 'graphql-scalars';
 
 // import userResolvers from "./resolvers/user";
 import { mergeTypeDefs } from '@graphql-tools/merge'
-import authResolvers from "./resolvers/auth";
-import apiariosResolvers from "./resolvers/apiario";
-import colmenaResolvers from "./resolvers/colmena";
-import zonaSugeridaResolvers from "./resolvers/zonaSugerida";
-import tareaResolvers from "./resolvers/tarea";
-import registroResolvers from "./resolvers/registro";
-import registroEspecificaResolvers from "./resolvers/registroEspecifica";
+// import authResolvers from "./resolvers/auth.js";
+import apiariosResolvers from "./resolvers/apiario.js";
+import colmenaResolvers from "./resolvers/colmena.js";
+import zonaSugeridaResolvers from "./resolvers/zonaSugerida.js";
+import tareaResolvers from "./resolvers/tarea.js";
+import registroResolvers from "./resolvers/registro.js";
+import registroEspecificaResolvers from "./resolvers/registroEspecifica.js";
 
 // import User from "./typeDefs/user";
-import Auth from "./typeDefs/auth";
-import Common from "./typeDefs/common";
-import Apiario from "./typeDefs/apiario";
-import Colmena from "./typeDefs/colmena";
-import ZonaSugerida from "./typeDefs/zonaSugerida";
-import Tarea from "./typeDefs/tarea";
-import Registro from "./typeDefs/registro";
+import Auth from "./typeDefs/auth.js";
+import Common from "./typeDefs/common.js";
+import Apiario from "./typeDefs/apiario.js";
+import Colmena from "./typeDefs/colmena.js";
+import ZonaSugerida from "./typeDefs/zonaSugerida.js";
+import Tarea from "./typeDefs/tarea.js";
+import Registro from "./typeDefs/registro.js";
 
 const Query = gql`
   type Query {
@@ -53,7 +53,6 @@ const typeDefs = mergeTypeDefs(types);
 
 const resolvers = lodash.merge(
   scalarResolvers,
-  authResolvers,
   apiariosResolvers,
   colmenaResolvers,
   zonaSugeridaResolvers,
